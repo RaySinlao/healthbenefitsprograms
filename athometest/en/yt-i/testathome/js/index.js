bc4a1ae9247931f4e6bb9157e2866435cd63af89
Justin Mealey <justinmealey@Justins-Mac-Pro.home>
2023-06-02 17:26:18 -0400

    Add click event for everflow

--- athometest/en/yt-i/testathome/js/index.js
+++ athometest/en/yt-i/testathome/js/index.js
@@ -76,6 +76,18 @@
 	window.clientId = null;
 	window.sessionId = null;
 	Dropzone.autoDiscover = false;
+	EF.click({
+		offer_id: EF.urlParameter('oid'),
+		affiliate_id: EF.urlParameter('affid'),
+		sub1: EF.urlParameter('sub1'),
+		sub2: EF.urlParameter('sub2'),
+		sub3: EF.urlParameter('sub3'),
+		sub4: EF.urlParameter('sub4'),
+		sub5: EF.urlParameter('sub5'),
+		uid: EF.urlParameter('uid'),
+		source_id: EF.urlParameter('source_id'),
+		transaction_id: EF.urlParameter('_ef_transaction_id'),
+	});
 	if (localStorage.getItem && localStorage.getItem('hbp_client')) {
 		clientId = localStorage.getItem('hbp_client');
 	} else {
